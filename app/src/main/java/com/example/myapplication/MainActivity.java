0c4685078a418f6338782c5eed65d5a2c6d3129f
kinney-x23 <kasiamatuszynska.kon@gmail.com>
2026-03-09 15:08:27 +0100

    Formularz goitowy

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,14 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +17,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextImie, editTextGatunek, editTextWaga;
+    Spinner spinnerGromada;
+    CheckBox checkBoxWymarle, checkBoxMesko, checkBoxRosliny;
+    RadioButton radioButtonDzien, radioButtonNoc;
+    Switch aSwitch;
+    SeekBar seekBar;
+    Button buttonDodaj;
+    TextView textViewKomunikat;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +38,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        editTextGatunek = findViewById(R.id.editTextText);
+        editTextImie = findViewById(R.id.editTextText2);
+        editTextWaga = findViewById(R.id.editTextNumber);
+        spinnerGromada = findViewById(R.id.spinner);
+        checkBoxWymarle = findViewById(R.id.checkBox);
+        checkBoxMesko = findViewById(R.id.checkBox2);
+        checkBoxRosliny = findViewById(R.id.checkBox3);
+        radioButtonDzien =  findViewById(R.id.radioButton);
+        radioButtonNoc =  findViewById(R.id.radioButton2);
+        aSwitch = findViewById(R.id.switch1);
+        seekBar = findViewById(R.id.seekBar);
+        buttonDodaj = findViewById(R.id.button);
+        textViewKomunikat = findViewById(R.id.textView8);
+
+
+
+
+
+
+
+
     }
 }
